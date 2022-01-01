67ff87b5007519e2501890dabf4bc37d10349784
Warsame Osman <osman18@my.yorku.ca>
2022-01-01 17:30:48 -0500

    a few changes to buy ticket page

--- src/pages/BuyTicketPage.js
+++ src/pages/BuyTicketPage.js
@@ -92,7 +92,7 @@ const [count,setCount] = useState(0);
           <select onChange={(e) => setQuantity(e.target.value)} className="form-select ml-3 mt-2" id="quantity" required>
     <option value="0">0</option>
     <option value="1">1</option>
-    <option value={quantity}>2</option>
+    <option value="2">2</option>
     <option value="3">3</option>
     <option value="4">4</option>
     <option value="5">5</option>
